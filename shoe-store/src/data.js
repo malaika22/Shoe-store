cc23d751ce1965cb867ef23c4993525f2da8ede9
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-01-13 01:35:00 +0500

    Added mocked data for Context Api

---
+++ shoe-store/src/data.js
@@ -0,0 +1,194 @@
+const data = [{
+    'new-release' : [{ 
+        'big-kids-shoe' :{
+            category : `Big Kids' Shoe`,
+            price: '$200' , 
+            title: 'Nike Air VaporMax 2020',
+            description : `Add some pep to your step in the Nike Air VaporMax 2020. 
+            The unique Air shape stretches from heel to toe for an out-of-this-world look that feels 
+            like you're walking on air. Flyknit construction wraps your foot in soft comfort 
+            and style that stands out amongst the crowd.` ,
+            img: './images/new-release6.jpg'
+        },
+        'men-training-shoe' :{
+            category: `Men's Training Shoe` ,
+            price: '$120' ,
+            title: 'Nike Free Metcon 3' , 
+            description: `The Nike Free Metcon 3 combines Nike Free flexibility 
+            around the forefoot with Metcon stability in the heel to help 
+            you get the most out of your training session.` , 
+            img: './images/newRelease/new-release5.jpg'
+        }, 
+
+        'women-running-shoe' : {
+            category: `Women's Running Shoe` , 
+            price: '$120' ,
+            title: `Nike Air Zoom Pegasus 37`,
+            description: `Reinvigorate your stride and running shoe game with the Nike Air Zoom Pegasus 37.
+            Pearlized accents and sweet pink colors bring a classy, elegant look to this icon. Delivering the same fit and feel that runners love, the shoe has an all-new forefoot cushioning unit and foam for optimal responsiveness.
+             The result is a durable, lightweight trainer designed for everyday running.`,
+             img: './images/newRelease/new-release4.jpg'
+        
+            } ,
+            'women-running-shoe1' : {
+                category: `Women's Running Shoe` , 
+                price: '$160',
+                title: 'Nike React Infinity Run Flyknit 2',
+                description: `The Nike React Infinity Run Flyknit 2 continues to help keep you running. 
+                A refreshed upper uses Flywire technology that combines with Flyknit for support and breathability 
+                where you need it. The high foam heights provide soft responsiveness and long-lasting comfort. 
+                It's still one of our most tested shoes, 
+                designed to help you feel the potential when your foot hits the pavement.`,
+                img: './images/newRelease/new-release3.jpg'
+
+            } ,
+            'men-hitt-class-shoe' :{
+                category : `Men's HIIT Class Shoe` ,
+                price: '$120',
+                title: `Nike Air Zoom SuperRep 2`,
+                description: `The Nike Air Zoom SuperRep 2 is designed for circuit training, HIIT, 
+                sprints and other fast-paced exercise. Layers of support team up with Zoom Air cushioning to 
+                keep your foot locked in and comfortable as you lunge, jump and push your way through every rep. 
+                A roomier design around the toes lets you get the perfect fit.` , 
+                img: './images/newRelease/new-release.jpg'
+            }
+
+    }]
+} ,
+{
+    'men-shoes' : [{ 
+        'nike-air-force-1-07' :{
+            category : `Men's Shoe`,
+            price: '$90' , 
+            title: `Nike Air Force 1 '07`,
+            description : `Add some pep to your step in the Nike Air VaporMax 2020. 
+            The unique Air shape stretches from heel to toe for an out-of-this-world look that feels 
+            like you're walking on air. Flyknit construction wraps your foot in soft comfort 
+            and style that stands out amongst the crowd.` ,
+            img: './images/men/men-1.jpg'
+        },
+        'nike-air-force-1-007' :{
+            category: `Men's Shoe` ,
+            price: '$120' ,
+            title: `Nike Air Force 1 '007`,
+            description: `The Nike Free Metcon 3 combines Nike Free flexibility 
+            around the forefoot with Metcon stability in the heel to help 
+            you get the most out of your training session.` , 
+            img: './images/men/men-2.jpg'
+        }, 
+
+        'nike-blazer-low-77-vintage' : {
+            category: `Men's Shoe` , 
+            price: '$85' ,
+            title: `Nike Blazer Low '77 Vintage`,
+            description: `Reinvigorate your stride and running shoe game with the Nike Air Zoom Pegasus 37.
+            Pearlized accents and sweet pink colors bring a classy, elegant look to this icon. Delivering the same fit and feel that runners love, the shoe has an all-new forefoot cushioning unit and foam for optimal responsiveness.
+             The result is a durable, lightweight trainer designed for everyday running.`,
+             img: './images/men/men-3.jpg'
+        
+            } ,
+            'nike-blazer-mid-77-vintage' : {
+                category: `Men's Shoe` , 
+                price: '$100',
+                title: `Nike Blazer Mid '77 Vintage`,
+                description: `The Nike React Infinity Run Flyknit 2 continues to help keep you running. 
+                A refreshed upper uses Flywire technology that combines with Flyknit for support and breathability 
+                where you need it. The high foam heights provide soft responsiveness and long-lasting comfort. 
+                It's still one of our most tested shoes, 
+                designed to help you feel the potential when your foot hits the pavement.`,
+                img: './images/men/men-4.jpg'
+
+            } ,
+            'nike-air-max-90' :{
+                category : `Men's Shoe` ,
+                price: '$120',
+                title: `Nike Air Max 90`,
+                description: `The Nike Air Zoom SuperRep 2 is designed for circuit training, HIIT, 
+                sprints and other fast-paced exercise. Layers of support team up with Zoom Air cushioning to 
+                keep your foot locked in and comfortable as you lunge, jump and push your way through every rep. 
+                A roomier design around the toes lets you get the perfect fit.` , 
+                img: './images/men/men-5.jpg'
+            } ,
+            'nike-air-max-90-premium' :{
+                category : `Men's Shoe` ,
+                price: '$160',
+                title: `Nike Air Max 90 Premium`,
+                description: `The Nike Air Zoom SuperRep 2 is designed for circuit training, HIIT, 
+                sprints and other fast-paced exercise. Layers of support team up with Zoom Air cushioning to 
+                keep your foot locked in and comfortable as you lunge, jump and push your way through every rep. 
+                A roomier design around the toes lets you get the perfect fit.` , 
+                img: './images/men/men-6.jpg'
+            }
+            
+
+    }]
+} ,
+{
+    'women-shoes' : [{ 
+        'nike-react-infinity-run-flyknit' :{
+            category : `Women's Running Shoe`,
+            price: '$160' , 
+            title: `Nike Air Force 1 '07`,
+            description : `Add some pep to your step in the Nike Air VaporMax 2020. 
+            The unique Air shape stretches from heel to toe for an out-of-this-world look that feels 
+            like you're walking on air. Flyknit construction wraps your foot in soft comfort 
+            and style that stands out amongst the crowd.` ,
+            img: './images/women-1.jpg'
+        },
+        'nike-space-hippie-04' :{
+            category: `Women's Shoe` ,
+            price: '$130' ,
+            title: `Nike Space Hippie 04`,
+            description: `The Nike Free Metcon 3 combines Nike Free flexibility 
+            around the forefoot with Metcon stability in the heel to help 
+            you get the most out of your training session.` , 
+            img: './images/women-2.jpg'
+        }, 
+
+        'nike-air-force-1-crater' : {
+            category: `Women's Shoe` , 
+            price: '$110',
+            title: `Nike Air Force 1 Crater`,
+            description: `Reinvigorate your stride and running shoe game with the Nike Air Zoom Pegasus 37.
+            Pearlized accents and sweet pink colors bring a classy, elegant look to this icon. Delivering the same fit and feel that runners love, the shoe has an all-new forefoot cushioning unit and foam for optimal responsiveness.
+             The result is a durable, lightweight trainer designed for everyday running.`,
+             img: './images/women-3.jpg'
+        
+            } ,
+            'nike-air-force-1-07-se' : {
+                category: `Women's Shoe` , 
+                price: '$100',
+                title: `Nike Air Force 1 '07 SE`,
+                description: `The Nike React Infinity Run Flyknit 2 continues to help keep you running. 
+                A refreshed upper uses Flywire technology that combines with Flyknit for support and breathability 
+                where you need it. The high foam heights provide soft responsiveness and long-lasting comfort. 
+                It's still one of our most tested shoes, 
+                designed to help you feel the potential when your foot hits the pavement.`,
+                img: './images/women-4.jpg'
+
+            } ,
+            'nike-blazer-low-se' :{
+                category : `Women's Shoe` ,
+                price: '$85',
+                title: `Nike Blazer Low SE`,
+                description: `The Nike Air Zoom SuperRep 2 is designed for circuit training, HIIT, 
+                sprints and other fast-paced exercise. Layers of support team up with Zoom Air cushioning to 
+                keep your foot locked in and comfortable as you lunge, jump and push your way through every rep. 
+                A roomier design around the toes lets you get the perfect fit.` , 
+                img: './images/women-5.jpg'
+            } ,
+            'nike-air-zoom-superrep-2' :{
+                category : `Women's HIIT Class` ,
+                price: '$120',
+                title: `Nike Air Zoom SuperRep 2`,
+                description: `The Nike Air Zoom SuperRep 2 is designed for circuit training, HIIT, 
+                sprints and other fast-paced exercise. Layers of support team up with Zoom Air cushioning to 
+                keep your foot locked in and comfortable as you lunge, jump and push your way through every rep. 
+                A roomier design around the toes lets you get the perfect fit.` , 
+                img: './images/women-6.jpg'
+            }
+            
+
+    }]
+}
+]
